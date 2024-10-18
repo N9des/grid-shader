cc6d13df59f6e28747c2b1ae56d508b858d75620
Félix Riffont <igele2000@gmail.com>
2024-10-18 12:08:08 +0200

    🗑️ Clean vertext shader

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -11,24 +11,10 @@ float cubicInOut(float t) {
 }
 
 void main() {
-		// Displacement
-		vec3 newPosition = position;
-		// float displacementIntensity = texture2D(uDisplacementTexture, uv).r;
-
-		// vec3 displacement = vec3(0.0, 0.0, 1.0);
-		// displacement *= displacementIntensity;
-		// displacement *= 3.0;
-
-		// newPosition += displacement;
-
-    // Final position
-    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
-
-		// Texture
-		// float textureIntensity = texture2D(uDisplacementTexture, uv).r;
 
     // Point size
     gl_PointSize = 0.4 * uResolution.y;

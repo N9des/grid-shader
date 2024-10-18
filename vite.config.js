23d491133bae103e7260e784d8629e5f2c3584b6
Félix Riffont <igele2000@gmail.com>
2024-10-18 11:20:30 +0200

    indent

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,17 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server: {
-        host: true,
-    },
-    build: {
-      outDir: '../dist',
-      emptyOutDir: true,
-      sourcemap: true
-    },
-    plugins: [
-      glslify()
-    ]
+  publicDir: '../static/',
+  base: './',
+  server: {
+      host: true,
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  plugins: [
+    glslify()
+  ]
 });
